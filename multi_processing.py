db1722f107f640bad0d61dfca75f35114c868e82
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-05-24 19:19:05 +0330

    pid,ppid

--- multi_processing.py
+++ multi_processing.py
@@ -2,14 +2,16 @@ from multiprocessing import Process,current_process
 
 import time 
 
-
+import os
 
 
 def show (name:str):
     print(f"starting  game {name}")
     time.sleep(3)
     print(f"Ending  game {name}")
-    print(current_process())
+    print(f"{name} pid",os.getpid()) # process id
+    print(f"{name} ppid",os.getppid()) # parent process id
+    print(f"{name}",current_process())
 
 
 # show("hassan")
@@ -18,7 +20,7 @@ def show (name:str):
 if __name__ == '__main__':    
     start = time.perf_counter()
 
-    p = Process(target=show, args=('hassan',))
+    p = Process(target=show, args=('hassan',),name = "hassan process")
     p1 = Process(target=show, args=('mmad',))
     p2 = Process(target=show, args=('ali',))
 
@@ -26,11 +28,14 @@ if __name__ == '__main__':
     p.start()
     p1.start()
     p2.start()
+    
+    print(p.is_alive()) # check now process run or not
 
     p.join() # finish process after run continue programe
     p1.join()
     p2.join()
 
+    print(p1.is_alive()) # check now process run or not
 
     end = time.perf_counter()
 
