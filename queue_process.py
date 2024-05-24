7c39c0b4d3f0a6c52bf96308200eb95cd50099d7
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-05-24 22:29:01 +0330

    Queue in process

--- queue_process.py
+++ queue_process.py
@@ -1,29 +1,32 @@
-from multiprocessing import Process
+from multiprocessing import Process ,Queue
 
 
-num = []
+numbers = []
 
-def func_process_1():
-    global num
+def func_process_1(queue):
+    num = queue.get()
     num.extend([1,2,3])
+    queue.put(num)
     print(num)
 
-def func_process_2():
-    global num
+def func_process_2(queue):
+    num = queue.get()
     num.extend([4,5,6])
+    queue.put(num)
     print(num)
     
 if __name__ == '__main__':    
-    
-    p1 = Process(target=func_process_1)
-    p2 = Process(target=func_process_2)
+    qs = Queue()
+    qs.put(numbers)
+    p1 = Process(target=func_process_1,args=(qs, ))
+    p2 = Process(target=func_process_2,args=(qs, ))
 
     p1.start()
     p2.start()
 
     p1.join()
     p2.join()
-    print(num)
+    print(qs.get())
     
     
     """result:
@@ -31,4 +34,5 @@ if __name__ == '__main__':
                 [4, 5, 6]
                 []
             process have split memory ---> we can connect memeory process with values and array 
+        after use Queue 
     """ 
