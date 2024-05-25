fb4cf3052beda862b06b1fd9f120f96dc902c3e7
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-05-25 22:56:59 +0330

    current_thread,enumerate

--- MultiThreading/thread.py
+++ MultiThreading/thread.py
@@ -1,7 +1,10 @@
 from time import sleep,perf_counter
-from threading import Thread
+from threading import Thread,current_thread,enumerate
 
 def show(name):
+    print(current_thread())
+    print(current_thread().getName,current_thread().ident)
+    print(enumerate())
     print(f"starting {name}")
     sleep(3)
     print(f"starting {name}")
@@ -9,21 +12,21 @@ def show(name):
 
 start = perf_counter()
 
-t1 = Thread(target=show,args=('hassan',),daemon=True)
-t2 = Thread(target=show,args=('mmad',),daemon=True)
+t1 = Thread(target=show,args=('hassan',),name='first',)
+t2 = Thread(target=show,args=('mmad',))
 
 
-print(t1.isDaemon(True))
-print(t2.isDaemon(True))
+# print(t1.isDaemon(True))
+# print(t2.isDaemon(True))
 
 t1.start()  
 t2.start() 
 
-# t1.join() # wait until thead terminate 
-# t2.join()
+t1.join() # wait until thead terminate 
+t2.join()
 
-print(t1.isDaemon())
-print(t2.isDaemon())
+# print(t1.isDaemon())
+# print(t2.isDaemon())
 
 end = perf_counter()
 print(end -start)
